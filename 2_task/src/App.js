0af8bf1ac6384ced27ca86ab443517b2e567f727
Florence <flo@82-128-128-12.rev.dnainternet.fi>
2020-03-25 14:29:20 +0200

    Updated with state

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -3,15 +3,25 @@ import "./App.css";
 import Person from "./Person/Person.js"
 
 class App extends Component {
+  state = {
+    persons: [
+      {name:"Florence",age:26,},
+      {name:"Patience",age:22,},
+      {name:"Jonathan",age:20,},
+      {name:"Rejoice",age:60,},
+      {name:"David",age:65,},
+    ]
+  };
+
+
   render () {
     return (
       <div>
-   
-    <Person name="Florence" age="26"/>
-    <Person name="Patience" age="20"/>
-    <Person name="Jonathan" age="23"/>
-    <Person name="David" age="65"/>
-    <Person name="Rejoice" age="60"/> 
+    <Person name= {this.state.persons[0].name} age={this.state.persons[0].age}/>
+    <Person name= {this.state.persons[1].name} age={this.state.persons[1].age}/>
+    <Person name= {this.state.persons[2].name} age={this.state.persons[2].age}/>
+    <Person name= {this.state.persons[3].name} age={this.state.persons[3].age}/>
+    <Person name= {this.state.persons[4].name} age={this.state.persons[4].age}/> 
   </div>
     )
   }
